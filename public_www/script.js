09d6dcff230da07b5c588837bf2a95db9f5a232c
websivustot <websivustot@gmail.com>
2019-06-01 09:35:43 +0300

    fixed errors

--- public_www/script.js
+++ public_www/script.js
@@ -1,10 +1,164 @@
+function createGrid(){    
+    clearCanvas();
+    ctx.strokeStyle = "#fff";
+    for(let i = 0; i < 20; i++){
+        let x = i * 25;
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, 500); 
+        ctx.stroke();  
+        ctx.beginPath();
+        ctx.moveTo(0, x);
+        ctx.lineTo(500, x);  
+        ctx.stroke();  
+    }    
+}
+
+function  createCells(){
+    let arr = new Array();    
+    for(let i = 0; i < 20; i++ ){
+        arr[i] = new Array();
+        for(let j = 0; j < 20; j++) {
+            arr[i][j] = 0;
+        }
+    }
+    return (arr);
+}
+
+function setCell(evt){    
+    let mousePos = getMousePos(evt);
+    let x = Math.ceil(mousePos.x/25)-1;
+    let y = Math.ceil(mousePos.y/25)-1;    
+     
+    if (cells[y][x] == 0) {
+        cells[y][x] = 1;
+        fillBoardCell(x,y);
+    } else {
+        cells[y][x] = 0;
+        clearBoardCell(x,y);
+    } 
+    console.log("x,y=",x,y,"check=",checkCell(x,y));
+
+
+}
+
+function setNext(x,y){    
+    if (next[y][x] == 0) {
+        fillBoardCell(x,y);
+    } else {        
+        clearBoardCell(x,y);
+    } 
+}
+
+function fillBoardCell(x,y){    
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(x*25,y*25,25,25); 
+    ctx.font='20px FontAwesome';
+    ctx.fillStyle = "#ff0000";
+    ctx.fillText('\uf188',x*25+3,y*25+20);   
+}
+
+function clearBoardCell(x,y){    
+    ctx.fillStyle = "green";
+    ctx.fillRect(x*25+1,y*25+1,23,23);      
+}
+
+function startStep(){
+    //console.log(cells);
+    for(let i = 0; i < 20; i++ ){
+        for(let j = 0; j < 20; j++) {
+            next[i][j] = checkCell(i,j);
+            setNext(i,j);
+        }
+        
+    }
+    //console.log(next);
+}
+
+function checkCell(x,y){
+    //console.log(cells);
+    let count = 0;
+    let x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8;
+    if (x == 0) {
+        x1 = 19;
+        x4 = 19;
+        x6 = 19;
+    } else {
+        x1 = x - 1;
+        x4 = x - 1;
+        x6 = x - 1;
+    }
+    if (y == 0) {
+        y1 = 19;
+        y2 = 19;
+        y3 = 19;
+    } else {
+        y1 = y - 1;
+        y2 = y - 1;
+        y3 = y - 1;
+    }
+    if (x == 19) {
+        x3 = 0;
+        x5 = 0;
+        x8 = 0;
+    } else {
+        x3 = x + 1;
+        x5 = x + 1;
+        x8 = x + 1;
+    }
+    if (y == 19) {
+        y6 = 0;
+        y7 = 0;
+        y8 = 0;
+    } else {
+        y6 = y + 1;
+        y7 = y + 1;
+        y8 = y + 1;
+    }
+    y4 = y;
+    y5 = y;
+    x2 = x;
+    x7 = x;
+    console.log(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8);
+    console.log(cells[y1][x1],cells[y2][x2],cells[y3][x3],cells[y4][x4],cells[y5][x5],
+        cells[y6][x6],cells[y7][x7],cells[y8][x8]);
+    if (cells[y1][x1] == 1) count = count + 1;
+    if (cells[y2][x2] == 1) count = count + 1;
+    if (cells[y3][x3] == 1) count = count + 1;
+    if (cells[y4][x4] == 1) count = count + 1;
+    if (cells[y5][x5] == 1) count = count + 1;
+    if (cells[y6][x6] == 1) count = count + 1;
+    if (cells[y7][x7] == 1) count = count + 1;
+    if (cells[y8][x8] == 1) count = count + 1;
+    
+    if ((count < 2) || (count > 3)) {
+        console.log("count false", count);
+        return 0;
+    } else {
+        if (cells[y][x] == 1) {
+            console.log("count true", count);
+            return 1;
+        } else {
+            if (count == 3) {
+                console.log("count true", count);
+                return 1;
+            } else {
+                console.log("count false", count);
+                return 0;
+            }
+            
+        }         
+    }
+}
+
 var canvas;
 var ctx;
 var running = 0;
 var interval;
-var cells = [];
-var next = [];
-var last = [];
+var cells = createCells();
+var next = cells;
+
+//console.log(cells);
 
 window.onload = function(){
     canvas = document.getElementById("mycanvas");
@@ -13,22 +167,9 @@ window.onload = function(){
     
 }
 
-function createGrid(){
-    cells=[];
-    clearCanvas();
-    ctx.strokeStyle = "#fff";
-    for(let i = 0; i < 20; i++){
-    let x = i * 25;
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, 500); 
-    ctx.stroke();  
-    ctx.beginPath();
-    ctx.moveTo(0, x);
-    ctx.lineTo(500, x);  
-    ctx.stroke();  
-    }    
-}
+
+
+
 
 function createRect(){
     let x = 0;
@@ -91,32 +232,7 @@ function getCoordinates(evt){
     writeMessage(message);
 }
 
-function setCell(evt){
-    let mousePos = getMousePos(evt);
-    let x = Math.ceil(mousePos.x/25)-1;
-    let y = Math.ceil(mousePos.y/25)-1;    
-    fillBoardCell(x,y); 
-    if( !contains(cells,{x:x,y:y,status:1}) ) {
-        if(!contains(cells,{x:x,y:y,status:0})){
-            cells.push({x:x,y:y,status:1});
-        }
-        else {
-            changeCell(cells,{x:x,y:y},1);
-        }
-    }   
-    
-    let neighb = setNeighbours(x,y); 
-    neighb.forEach(function(item,i,neighb){        
-        active = {x:item.x,y:item.y,status:1};        
-        if( !contains(cells,item)&&!contains(cells,active) ) {
-            cells.push(item);
-        }
-    })
-    //console.log(cells);
-    //console.log(neighb);
-    let message = "Mouse position: "+x+", "+y;
-    //writeMessage(message);
-}
+
 
 function contains(arr, elem) {
     for (var i = 0; i < arr.length; i++) {        
@@ -138,7 +254,7 @@ function changeCell(arr, elem, value) {
 }
 
 function checkNeighbours (neighb) {
-    console.log("neighb",neighb);
+    //console.log("neighb",neighb);
     let amount = 0;
     for(item in neighb){
         if (item.status === 1) {
@@ -158,24 +274,14 @@ function lifeCycle(cells){
         if (amount !== 2) {            
             changeCell(cells,cells[i],0);
         }
-        console.log(cells[i],amount);
+        //console.log(cells[i],amount);
     }
 }
 
-function startStep(){
-    last = cells;
-    lifeCycle(cells);
-
-}
 
 
-function fillBoardCell(x,y){    
-    ctx.fillStyle = "#fff";
-    ctx.fillRect(x*25,y*25,25,25); 
-    ctx.font='20px FontAwesome';
-    ctx.fillStyle = "#ff0000";
-    ctx.fillText('\uf188',x*25+3,y*25+20);   
-}
+
+
 
 function setNeighbours(x,y){
     let neighbours = [];    
